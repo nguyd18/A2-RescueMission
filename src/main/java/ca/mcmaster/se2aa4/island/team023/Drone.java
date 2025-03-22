63b869e786d14d052baf5da1cdfea54fb530b43a
Daniel Nguyen <nguyd18@mcmaster.ca>
2025-03-21 22:08:24 -0400

    wrote some comments/pseudocode about the first part of the grid-search

--- src/main/java/ca/mcmaster/se2aa4/island/team023/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team023/Drone.java
@@ -1,30 +1,23 @@
 package ca.mcmaster.se2aa4.island.team023;
 
-
 import java.util.ArrayDeque;
 import java.util.Queue;
-
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 import ca.mcmaster.se2aa4.island.team023.Heading.HeadingStates;
 
-
 public class Drone extends Aircraft {
-   
     private IMap map = new Map();
     private Point<Integer> relativePos;
     private Logger logger = LogManager.getLogger();
-    private boolean groundDetected = false;
+    private boolean groundDetected;
     private boolean firstRun;
 
-
     private Queue<JSONObject> actions = new ArrayDeque<>();
-
 
     public Drone(String heading, int fuelCap) {
         super(heading, fuelCap);
@@ -32,7 +25,6 @@ public class Drone extends Aircraft {
         firstRun = true;
         groundDetected = false;
     }
-
 
     /*
      * Called every game loop, this method returns a JSONObject representing the drone's chosen action
@@ -43,20 +35,18 @@ public class Drone extends Aircraft {
             firstRun = false;
         }
 
-
-        // abort after 50 iterations to stop test drone from flying off the map
-        // otherwise take the next action in the queue is there is one
-
-
         // if action remaining in queue execute
         if (!actions.isEmpty()) return actions.remove();
 
-
         // actions done per cell
-        if (groundDetected) actions.add(scan());
-        if (!groundDetected) actions.add(radar(heading.getHeadingState().next()));
-        actions.add(forward());
+        if (groundDetected){ 
+            actions.add(scan());
+        } 
+        if (!groundDetected) {
+            actions.add(radar(heading.getHeadingState().next()));
+        }
 
+        actions.add(forward());
 
         return actions.remove();
     }
@@ -66,29 +56,43 @@ public class Drone extends Aircraft {
      */
     public void update(JSONObject response) {
         map.placeCell(relativePos.x(), relativePos.y(), response);
-        //echo
-        if (!groundDetected){
-            try {
-                if (response.getJSONObject("extras").get("found").equals("GROUND")) {
-                    groundDetected = true;
-                    actions.clear();
-                    actions.add(turnRight());
-                }
-            } catch (JSONException e) {
-                logger.error(e.getMessage());
-            }
-        }
+		// update battery
+		updateBattery(response);
+        
+        // Step 1: If the drone detects the island, turn right to face the island
         try {
-            if (!response.getJSONObject("extras").getJSONArray("biomes").get(0).equals("OCEAN")){
+            if (!groundDetected && response.getJSONObject("extras").get("found").equals("GROUND")) {
+                groundDetected = true;
                 actions.clear();
-                actions.add(stop());
+                actions.add(turnRight());
+            }
+        } catch (JSONException e) {
+            logger.error(e.getMessage());
+        }
+        
+        // continue to fly until the drone scans the island
+        // once the island is found, the drone will continue to fly until it reaches the edge of the island
+        // once the edge is reached (drones scans ocean), stop drone
+        boolean isIsland = false;
+        try {
+            if (!isIsland && response.getJSONObject("extras").getJSONArray("biomes").get(0).equals("OCEAN")){
+                actions.clear();
+                actions.add(forward());
             }
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
+
+        // stop if not on ocean
+        // try {
+        //     if (!response.getJSONObject("extras").getJSONArray("biomes").get(0).equals("OCEAN")){
+        //         actions.clear();
+        //         actions.add(stop());
+        //     }
+        // } catch (Exception e) {
+        //     logger.error(e.getMessage());
+        // }
     }
-
-
    
     // Action to fly forward
     protected JSONObject forward() {
@@ -98,14 +102,11 @@ public class Drone extends Aircraft {
         return action;
     }
 
-
     protected JSONObject turnLeft() {
-       
         // TODO encapsulate this
         relativePos = Point.addInts(relativePos, heading.getHeadingState().getNextPoint());
         heading.turnCounterClockwise();
         relativePos = Point.addInts(relativePos, heading.getHeadingState().getNextPoint());
-
 
         JSONObject action = new JSONObject();
         JSONObject direction = new JSONObject();
@@ -113,36 +114,28 @@ public class Drone extends Aircraft {
         direction.put("direction", heading.getHeadingState().toString());
         action.put("parameters", direction);
 
-
         return action;
     }
 
-
     protected JSONObject turnRight() {
-
 
         // TODO encapsulate this
         relativePos = Point.addInts(relativePos, heading.getHeadingState().getNextPoint());
         heading.turnClockwise();
         relativePos = Point.addInts(relativePos, heading.getHeadingState().getNextPoint());
 
-
         JSONObject action = new JSONObject();
         JSONObject direction = new JSONObject();
         action.put("action", "heading");
         direction.put("direction", heading.getHeadingState().toString());
         action.put("parameters", direction);
 
-
         return action;
     }
 
-
     protected JSONObject radar(HeadingStates dir) {
 
-
         // assert dir != heading.getHeadingState().next().next() : "Invalid echo. Drone tried to send radar backwards";
-
 
         JSONObject action = new JSONObject();
         JSONObject direction = new JSONObject();
@@ -150,10 +143,7 @@ public class Drone extends Aircraft {
         direction.put("direction", dir.toString());
         action.put("parameters", direction);
         return action;
-
-
     }
-
 
     protected JSONObject scan() {
         JSONObject action = new JSONObject();
@@ -161,13 +151,11 @@ public class Drone extends Aircraft {
         return action;
     }
 
-
     protected JSONObject stop() {
         JSONObject action = new JSONObject();
         action.put("action", "stop");
         return action;
     }
-
 
 	protected void updateBattery(JSONObject response) {
 		try {
@@ -178,8 +166,4 @@ public class Drone extends Aircraft {
 			logger.error(e.getMessage());
 		}
 	}
-
 }
-
-
-
